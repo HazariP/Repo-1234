8b76cb4fe88ff938cebf132e2d0f717187335097
HazariP <pradnyakshatriya2010@gmail.com>
2022-02-28 15:08:55 +1300

    first commit

---
+++ Repo-1234/Program.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Repo_1234
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // open chrome browser
+            IWebDriver driver = new ChromeDriver();
+
+
+
+            // launch turnup browser
+            driver.Navigate().GoToUrl("http://horse.industryconnect.io/Account/Login?ReturnUrl=%2f");
+
+            // identify username textbox and enter valid username
+            IWebElement usernameTextbox = driver.FindElement(By.Id("UserName"));
+            usernameTextbox.SendKeys("hari");
+
+           // identify password textbox and enter valid password
+            IWebElement passwordTextbox = driver.FindElement(By.Id("Password"));
+            passwordTextbox.SendKeys("123123");
+
+            // click on login button
+            IWebElement loginButton = driver.FindElement(By.XPath("//*[@id='loginForm']/form/div[3]/input[1]"));
+                loginButton.Click();
+
+
+            //check if user name is logged in successfully 
+        }
+    }
+}
